0af850fbf8f5baafbee137d2e2c6adf14fe9d946
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-01-06 16:42:40 +0100

    solution plus simple sans env

--- config/conn.js
+++ config/conn.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-// On charge les variables d'environnement
-require("dotenv").config();
 
 const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(
+      "mongodb+srv://mohamedaminenamasse_db_user:mXr605umJgGwxSBQ@cluster0.41ovz2h.mongodb.net/test?retryWrites=true&w=majority"
+    )
     .then(() => console.log("Connexion à MongoDB réussie !"))
     .catch((err) => console.log("Connexion à MongoDB échouée !", err));
 };
