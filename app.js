76c9d0e5eb63307a1899d6d623fd4e8c80fb952a
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-01-06 13:35:22 +0100

    commit

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 
 const Thing = require("./models/Things");
-const mongoose = require("mongoose");
 const Recipe = require("./models/Recipes");
 const User = require("./models/Users");
+const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://mohamedaminenamasse_db_user:aII8GpHMxyqFBy9D@cluster0.41ovz2h.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://mohamedaminenamasse_db_user:QlpFqRlUFiYhDHgd@cluster0.41ovz2h.mongodb.net/test?retryWrites=true&w=majority"
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
@@ -59,4 +59,42 @@ app.get("/api/stuff", (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+// --- ROUTES POUR LES RECETTES ---
+
+// Ajouter une recette (POST)
+app.post("/api/recettes", (req, res) => {
+  const recipe = new Recipe({
+    ...req.body, // Récupère title, ingredients, etc. depuis le corps de la requête
+  });
+
+  recipe
+    .save()
+    .then(() => res.status(201).json({ message: "Recette enregistrée !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+// Récupérer toutes les recettes (GET)
+app.get("/api/recettes", (req, res) => {
+  Recipe.find()
+    .then((recettes) => res.status(200).json(recettes))
+    .catch((error) => res.status(400).json({ error }));
+});
+//Récuperer une recette par ID (GET)
+app.get("/api/recettes/:id", (req, res) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+});
+//Modifier une recette (PUT)
+app.put("/api/recettes/:id", (req, res) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Recette modifiée !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+//Supprimer une recette (DELETE)
+app.delete("/api/recettes/:id", (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
 module.exports = app;
