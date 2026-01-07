a9411dccf10e170bcffca6da572113fd0a9ada76
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-01-07 10:58:35 +0100

    ajout middleware auth

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const auth = require("../middleware/auth");
 const recipeCtrl = require("../controllers/recipe");
 
 router.get("/", recipeCtrl.getAllRecipes);
