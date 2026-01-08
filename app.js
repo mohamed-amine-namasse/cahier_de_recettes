e4a3401b0958fc90b7a80edeff9c2329f11ba245
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-01-08 09:19:32 +0100

    front retabli

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 //app.use(bodyParser.json());
 
-app.use("/api/recipe", recipeRoutes);
+app.use("/api/stuff", recipeRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/picture", pictureRoutes);
 app.use("/images", express.static(path.join(__dirname, "images")));
--- routes/recipe.js
+++ routes/recipe.js
@@ -4,8 +4,8 @@ const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 const recipeCtrl = require("../controllers/recipe");
 
-router.get("/", auth, recipeCtrl.getAllRecipes);
-router.post("/", auth, multer, recipeCtrl.createRecipe);
+router.get("/", recipeCtrl.getAllRecipes);
+router.post("/", multer, recipeCtrl.createRecipe);
 router.get("/:id", auth, recipeCtrl.getOneRecipe);
 router.put("/:id", auth, recipeCtrl.modifyRecipe);
 router.delete("/:id", auth, recipeCtrl.deleteRecipe);
