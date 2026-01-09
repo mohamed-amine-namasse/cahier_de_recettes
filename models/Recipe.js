064f9795937d9668b4e7bd997279f45237302195
mohamed-amine-namasse <namasse.medamine@laplateforme.io>
2026-01-09 13:34:46 +0100

    correction sur type steps

--- models/Recipe.js
+++ models/Recipe.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const recipeSchema = mongoose.Schema({
   title: { type: String, required: true },
   ingredients: { type: String, required: true },
-  steps: { type: Number, required: true },
+  steps: [{ type: String, required: true }],
   author: { type: String, required: true },
   date: { type: Date, required: true },
   userId: { type: String, required: true },
